7f3103ac318f43527fd0da50c7fcfed2b1a2002f
MuhamedRagab <mr2837465@gmail.com>
2023-09-10 23:00:23 +0300

    Edit: Theme and seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Amiri_Quran } from "next/font/google";
+import { Amiri_Quran } from "next/font/google";
 import Navbar from "./components/layout/Navbar";
 import ScrollYProgress from "./components/layout/ScrollYProgress";
 
@@ -11,6 +11,23 @@ export const amiri_quran = Amiri_Quran({
 
 export const metadata: Metadata = {
   title: "Quran App",
+  icons: [
+    {
+      href: "/favicon.ico",
+      sizes: "32x32",
+      type: "image/png",
+      url: "/favicon.ico",
+    },
+  ],
+  authors: [
+    {
+      name: "Mohamed Ragab",
+      url: "https://quran-mr.vercel.app",
+    },
+  ],
+  applicationName: "Quran App",
+  keywords: ["Quran", "Quran App", "Quran App for listening and reading Quran"],
+  creator: "Mohamed Ragab",
   description: "Quran App for listening and reading Quran",
 };
 
@@ -20,7 +37,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={amiri_quran.className}>
         <ScrollYProgress />
         <Navbar />
