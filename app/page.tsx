ee4fffdd4e00739d1814f28192a5053ad26e85dd
MuhamedRagab <mr2837465@gmail.com>
2023-09-11 11:18:02 +0300

    Update: Get surahs from meta api

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,52 @@
 "use client";
 
-import { useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import SurahList from "./home/SurahList";
 import surahData from "quran-db/utils/surah_data";
 import SearchSurah from "./home/SearchSurah";
 import { twMerge } from "tailwind-merge";
+import Loader from "./components/Loader";
 
 export interface ISurahListProps {
-  id: number;
-  arabic: string;
-  aya: number;
+  number: number;
   name: string;
-  english: string;
-  place: string;
-  turkish: string;
+  englishName: string;
+  englishNameTranslation: string;
+  numberOfAyahs: number;
+  revelationType: string;
 }
 
 export default function Home() {
-  const [surah, setSurah] = useState(surahData);
+  const [surah, setSurah] = useState<ISurahListProps[] | []>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_QURAN_API}/meta`)
+          .then((res) => res.json())
+          .then(({ data }) => {
+            setSurah(data.surahs.references);
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   return (
     <main
       className={twMerge(
-        "prose flex flex-col items-center mx-auto p-4",
+        "prose flex flex-col items-center mx-auto p-2 sm:p-4",
         "max-w-screen-lg"
       )}
     >
       <section className="container flex flex-col items-center justify-center">
-        <SearchSurah setSurah={setSurah} />
-        <SurahList surah={surah} />
+        <div className="flex items-center gap-6">
+          <SearchSurah setSurah={setSurah} />
+        </div>
+        <Suspense fallback={<Loader />}>
+          <SurahList surah={surah} />
+        </Suspense>
       </section>
     </main>
   );
