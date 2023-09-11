b8ea2ab7342ef81094b3217019f6482c96eefe18
MuhamedRagab <mr2837465@gmail.com>
2023-09-11 15:47:26 +0300

    Set loader when no data

--- app/home/SurahList.tsx
+++ app/home/SurahList.tsx
@@ -2,6 +2,7 @@ import { ISurahListProps } from "../page";
 import Link from "next/link";
 import Aos from "aos";
 import { useEffect } from "react";
+import Loader from "../components/Loader";
 
 export default function SurahList({ surah }: { surah: ISurahListProps[] }) {
   useEffect(() => {
@@ -11,31 +12,30 @@ export default function SurahList({ surah }: { surah: ISurahListProps[] }) {
     });
   }, []);
 
+  if (surah.length === 0) return <Loader />;
+
   return (
     <ul
       className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 w-full place-items-center pl-0"
       dir="rtl"
     >
-      {surah.length > 0 &&
-        surah.map(
-          ({ name, number, numberOfAyahs, englishName, revelationType }) => (
-            <li
-              data-aos="zoom-in"
-              key={number}
-              className="flex flex-col p-3 rounded text-center border border-slate-700 w-full hover:bg-slate-700 hover:text-white transition duration-200"
-            >
-              <Link
-                href={`/surah/${number}`}
-                className="link link-secondary transition decoration-transparent mb-2 text-xl"
-              >
-                {name}
-              </Link>
-              <small>English: {englishName}</small>
-              <small>Place: {revelationType}</small>
-              <small>{numberOfAyahs} Ayahs</small>
-            </li>
-          )
-        )}
+      {surah.map(({ name, id, aya, arabic, place }) => (
+        <li
+          data-aos="zoom-in"
+          key={id}
+          className="flex flex-col p-3 rounded text-center border border-slate-700 w-full hover:bg-slate-700 hover:text-white transition duration-200"
+        >
+          <Link
+            href={`/surah/${id}`}
+            className="link link-secondary transition decoration-transparent mb-2 text-xl"
+          >
+            {arabic}
+          </Link>
+          <small>English: {name}</small>
+          <small>Place: {place}</small>
+          <small>{aya} Ayahs</small>
+        </li>
+      ))}
     </ul>
   );
 }
