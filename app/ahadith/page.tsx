da8e754a1c33b1fce845b180d4fef6779e0a9c9b
MuhamedRagab <mr2837465@gmail.com>
2023-09-14 13:31:37 +0300

    Remove AhadithSearch

--- app/ahadith/page.tsx
+++ app/ahadith/page.tsx
@@ -1,21 +1,14 @@
-"use client";
-import { useState } from "react";
 import Container from "../components/Container";
 import AhadithList from "./components/AhadithList";
-import AhadithSearch from "./components/AhadithSearch";
-import ahadithData from "@/app/data/ahadith.json";
 
 export default function AhadithPage() {
-  const [ahadith, setAhadith] = useState(ahadithData);
-
   return (
     <Container
       className="prose flex flex-col justify-center items-center"
       isRtl={true}
     >
       <h1>أحاديث نبويه</h1>
-      <AhadithSearch setAhadith={setAhadith} />
-      <AhadithList ahadith={ahadith} />
+      <AhadithList />
     </Container>
   );
 }
