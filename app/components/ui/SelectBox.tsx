16dab199281d9ecc3aec77a5b2d2b63f66eb9732
MuhamedRagab <mr2837465@gmail.com>
2023-09-14 13:33:12 +0300

    Create SelectBox.tsx

---
+++ app/components/ui/SelectBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface Props {
+  label?: string;
+  options: string[];
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  [key: string]: any;
+}
+
+export default function SelectBox({ options, label, onChange }: Props) {
+  return (
+    <div>
+      {label && (
+        <label className="label" dir="rtl">
+          <span className="label-text">{label}</span>
+        </label>
+      )}
+      <select
+        className="select select-accent w-full max-w-xs"
+        onChange={onChange}
+      >
+        {options.map((opt) => (
+          <option value={opt} key={opt} className="capitalize">
+            {opt}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
